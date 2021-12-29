cba53003cede152e9085a2e5f67303b879b172c2
youkeyao <youkeyao@sjtu.edu.cn>
2021-12-29 17:02:41 +0800

    update evaluation script

--- evaluation_local.py
+++ evaluation_local.py
@@ -9,37 +9,75 @@ from torch.distributions import Categorical
 import os
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
-
-def get_actions(state, algo, indexs):
-
-    # random agent
-    actions = np.random.randint(4, size=3)
-
-    # rl agent
-    if algo == 'rl':
-        obs = get_observations(state, indexs, obs_dim=26, height=10, width=20)
-        logits = agent.choose_action(obs)
-        logits = torch.Tensor(logits)
-        actions = np.array([Categorical(out).sample().item() for out in logits])
-
-    return actions
+def get_valid_agents():
+    dir_path = os.path.join(os.path.dirname(__file__), 'agent')
+    return [f for f in os.listdir(dir_path) if f != "__pycache__"]
 
 
-def get_join_actions(obs, algo_list):
-    obs_2_evaluation = obs[0]
-    indexs = [0,1,2,3,4,5]
-    first_action = get_actions(obs_2_evaluation, algo_list[0], indexs[:3])
-    second_action = get_actions(obs_2_evaluation, algo_list[1], indexs[3:])
-    actions = np.zeros(6)
-    actions[:3] = first_action[:]
-    actions[3:] = second_action[:]
-    return actions
+def get_players_and_action_space_list(g):
+    if sum(g.agent_nums) != g.n_player:
+        raise Exception("agent number = %d 不正确，与n_player = %d 不匹配" % (sum(g.agent_nums), g.n_player))
+
+    n_agent_num = list(g.agent_nums)
+    print("n_agent_num: ", n_agent_num)
+    for i in range(1, len(n_agent_num)):
+        n_agent_num[i] += n_agent_num[i - 1]
+
+    # 根据agent number 分配 player id
+    players_id = []
+    actions_space = []
+    for policy_i in range(len(g.obs_type)):
+        if policy_i == 0:
+            players_id_list = range(n_agent_num[policy_i])
+        else:
+            players_id_list = range(n_agent_num[policy_i - 1], n_agent_num[policy_i])
+        players_id.append(players_id_list)
+
+        action_space_list = [g.get_single_action_space(player_id) for player_id in players_id_list]
+        actions_space.append(action_space_list)
+
+    return players_id, actions_space
 
 
-def run_game(env, algo_list, episode, verbose=False):
+def get_joint_action_eval(game, multi_part_agent_ids, policy_list, actions_spaces, all_observes):
+    if len(policy_list) != len(game.agent_nums):
+        error = "模型个数%d与玩家个数%d维度不正确！" % (len(policy_list), len(game.agent_nums))
+        raise Exception(error)
+
+    # [[[0, 0, 0, 1]], [[0, 1, 0, 0]]]
+    joint_action = []
+    for policy_i in range(len(policy_list)):
+
+        agents_id_list = multi_part_agent_ids[policy_i]
+
+        action_space_list = actions_spaces[policy_i]
+        function_name = 'm%d' % policy_i
+        for i in range(len(agents_id_list)):
+            agent_id = agents_id_list[i]
+            a_obs = all_observes[agent_id]
+            each = eval(function_name)(a_obs, action_space_list[i], game.is_act_continuous)
+            joint_action.append(each)
+    return joint_action
+
+
+def run_game(env, multi_part_agent_ids, actions_space, algo_list, episode, verbose=False):
 
     total_reward = np.zeros(6)
     num_win = np.zeros(3)
+
+    for i in range(len(algo_list)):
+        if algo_list[i] not in get_valid_agents():
+            raise Exception("agents {} not valid!".format(algo_list[i]))
+
+        file_path = os.path.dirname(os.path.abspath(__file__)) + "/agent/" + algo_list[i] + "/submission.py"
+        if not os.path.exists(file_path):
+            raise Exception("file {} not exist!".format(file_path))
+        import_path = '.'.join(file_path.split('/')[-3:])[:-3]
+        function_name = 'm%d' % i
+        import_name = "my_controller"
+        import_s = "from %s import %s as %s" % (import_path, import_name, function_name)
+        print(import_s)
+        exec(import_s, globals())
 
     for i in range(1, episode + 1):
         episode_reward = np.zeros(6)
@@ -49,9 +87,9 @@ def run_game(env, algo_list, episode, verbose=False):
         step = 0
 
         while True:
-            joint_action = get_join_actions(state, algo_list)
+            joint_action = get_joint_action_eval(env, multi_part_agent_ids, algo_list, actions_space, env.all_observes)
 
-            next_state, reward, done, _, info = env.step(env.encode(joint_action))
+            next_state, reward, done, _, info = env.step(joint_action)
             reward = np.array(reward)
             episode_reward += reward
 
@@ -71,6 +109,7 @@ def run_game(env, algo_list, episode, verbose=False):
 
             state = next_state
             step += 1
+        print(str(i) + '/' + str(episode))
 
         total_reward += episode_reward
 
@@ -100,4 +139,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     agent_list = [args.my_ai, args.opponent]
-    run_game(game, algo_list=agent_list, episode=args.episode, verbose=False)
+    multi_part_agent_ids, actions_space = get_players_and_action_space_list(game)
+    run_game(game, multi_part_agent_ids, actions_space, algo_list=agent_list, episode=args.episode, verbose=False)
